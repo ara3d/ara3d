cb7b1a9c65b2a33953e6f77fe63d2fab84515632
Christopher Diggins <cdiggins@gmail.com>
2023-11-02 14:40:17 -0400

    Working on noise routines taken from Unity.Mathematics

---
+++ labs/Ara3D.Noise/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Ara3D.Noise
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ labs/Ara3D.Noise/PropertyAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Unity.Mathematics
+{
+    /// <summary>
+    /// Used by property drawers when vectors should be post normalized.
+    /// </summary>
+    public class PostNormalizeAttribute : Attribute {}
+
+    /// <summary>
+    /// Used by property drawers when vectors should not be normalized.
+    /// </summary>
+    public class DoNotNormalizeAttribute : Attribute {}
+}
