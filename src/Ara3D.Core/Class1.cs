0bb088975bdf6a2369d8a2e07924bcda18cfbc4a
Christopher Diggins <cdiggins@gmail.com>
2023-11-07 20:28:34 -0500

    Now finally copying the nuget packages after build

---
+++ src/Ara3D.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ara3D.Core
+{
+    public static class Test
+    {
+        public static void WriteLine()
+        {
+            Console.WriteLine($"Hello world");
+        }
+    }
+}
