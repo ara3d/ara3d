f05f617104490b490a36eeb266cb07719f031be7
Christopher Diggins <cdiggins@gmail.com>
2024-01-10 14:23:18 -0500

    Assuring Plato output compiles

--- devops/push-plato.bat
+++ devops/push-plato.bat
@@ -1,9 +1,15 @@
+echo off
 call :subroutine "%~dp0\..\plato" %1
+git status
+git add .
+git commit -m %1
+git push
 exit /b
 
 rem <Directory> <message>
 :subroutine
 pushd %1 
+git status
 git add .
 git commit -m %2
 git push
