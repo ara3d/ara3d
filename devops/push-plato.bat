2221632ba39d079c10defa962a0d8a6f7483fc68
Christopher Diggins <cdiggins@gmail.com>
2024-01-13 15:27:24 -0500

    Making devops script more verbose

--- devops/push-plato.bat
+++ devops/push-plato.bat
@@ -2,8 +2,10 @@ echo off
 call :subroutine "%~dp0\..\plato" %1
 git status
 git add .
+git status
 git commit -m %1
 git push
+git status
 exit /b
 
 rem <Directory> <message>
@@ -11,7 +13,9 @@ rem <Directory> <message>
 pushd %1 
 git status
 git add .
+git status
 git commit -m %2
 git push
+git status
 popd
 exit /b
