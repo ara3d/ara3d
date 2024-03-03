83826029922760fa05fea5a657674673f055693d
Christopher Diggins <cdiggins@gmail.com>
2024-03-03 15:27:45 -0500

    Tag experiment

--- devops/push.bat
+++ devops/push.bat
@@ -1,10 +1,10 @@
-echo off
+rem echo off
 if "%~1" == "" (echo "No message provided" && exit /b)
 call :subroutine "%~dp0\..\domo" %1 %2
 call :subroutine "%~dp0\..\plato" %1 %2 
 call :subroutine "%~dp0\..\parakeet" %1 %2
 call :subroutine "%~dp0\..\bowerbird" %1 %2
-call :subroutine "%~dp0\.." %1
+call :subroutine "%~dp0\.." %1 %2
 exit /b
 
 rem <Directory> <message>
@@ -18,8 +18,8 @@ git commit -m %2
 git push
 git status
 rem If a version is provided, then make a tag. 
-if not "%3" == "" (
-	git tag -a v%3 -m "Version %3 created %2"
+if not "%3"=="" (
+	git tag -a v%3 -m "Version %3 created %~2"
 	git push origin --tags
 )
 popd
