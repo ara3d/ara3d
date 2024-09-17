4783f053b87631295d009b2c169015123821fb3a
Christopher Diggins <cdiggins@gmail.com>
2024-09-17 09:36:06 -0400

    Adding IFC-toolkit to list of submodules to push, and udpating readme

--- devops/push.bat
+++ devops/push.bat
@@ -10,6 +10,7 @@ call :subroutine "%~dp0\..\mathematics" %1 %2
 call :subroutine "%~dp0\..\geometry" %1 %2
 call :subroutine "%~dp0\..\geometry-toolkit" %1 %2
 call :subroutine "%~dp0\..\Plato.Geometry" %1 %2
+call :subroutine "%~dp0\..\IFC-toolkit" %1 %2
 call :subroutine "%~dp0\.." %1 %2
 exit /b
 
