6adb71231b53fa07d8b7cc2d71eeb6e949026ee4
Christopher Diggins <cdiggins@gmail.com>
2024-11-03 16:31:31 -0500

    WIP improvements to geometry library

---
+++ labs/TopologyHack/UnitTest1.cs
@@ -0,0 +1,130 @@
+using Plato.DoublePrecision;
+
+namespace TopologyHack
+{
+    public void TestJsonLoader()
+    {
+        var json = 
+    }
+
+    /*
+    public class Site
+    {
+        public List<Building> Buildings { get; set;} = new List<Building>();
+        public Bounds3D? Bounds { get; set; }
+    }
+
+    public class BuildingType
+    {
+        public string Name { get; set; }
+    }
+
+    public class Building
+    {
+        public BuildingType Type { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public List<Floor> Floors { get; set; } = new List<Floor>();
+    }
+
+    public class Floor 
+    {
+        public string Name;
+        public string Address;
+        public List<Space> Spaces { get; set; } = new List<Space>();
+    }
+
+    public class SpaceType
+    {
+        public string Name { get; set; }
+        public string Use { get; set; }
+    }
+
+    public class Space
+    {
+        public List<Room> Rooms { get; set; } = new List<Room>();
+    }
+
+    public class RoomType
+    {
+        public string Name { get; set; }
+        public string Use { get; set; }
+    }
+
+    public class Room 
+    {
+        public Vector2D Location { get; set; }
+        public RoomType Type { get; set;}
+        public string? Name { get; set; }
+        public double? Area { get; set; }
+        public double? MinArea { get; set; }
+        public double? MaxArea { get; set; }
+        public double? MinSide { get; set; }
+        public double? MaxSide { get; set; }
+        public Bounds3D Bounds { get; set; }
+    }
+
+    public class Wall
+    {
+        public Room Room { get; set; }
+    }
+
+    public class RoomConstraint
+    {
+    }
+
+    public class RoomDistance : Room
+    {
+    }
+    
+    /*
+    public enum Things
+    {
+        Building,
+        Space,
+        Area,
+        Room,
+        Level,
+        Wall,
+        Slab, 
+        Corridor,
+        Massing,
+        Envelope,
+        Window,
+        Door,
+        Core,
+        Shell,
+        Boundary, 
+    }
+
+    public enum Attributes
+    {
+        Dimensions,
+        Area,
+    }
+
+    public class Outline
+    {
+    } 
+    
+    public class AbstractTopology
+    {
+    }
+
+    public enum ConstraintType
+    {
+        AlignToSurface,
+        AlignToCurve,
+        AlignToLine,
+        AttachToSurface,
+        MaintainDistance, 
+        NoCloserThan,
+        NoFurtherThan,
+    }
+    
+    public class Tests
+    {
+       
+    }
+    */
+}
